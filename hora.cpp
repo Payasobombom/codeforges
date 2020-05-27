6e7200893091e3d463c9e9d5da3ded1f58dbd862
Payasobombom <luizaga_18lm@hotmail.com>
2020-05-26 21:02:01 -0400

    Problema de hora

---
+++ hora.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main ()
+
+{
+int t,h,m;
+scanf("%d",&t)
+
+    for (int i=0;i>=t;i=i+1)
+    {
+        scanf("%d",&h);
+        scanf("%d",&m);
+            if ((h=< 0 && h < 24 && m =<0 && m < 60 )
+            {
+                h= 23 - h;
+                m= 60 - m;
+                    if (h==0)
+                     { 
+                        
+                       printf("%d\n",m)
+                    }  
+                    else
+                    {
+                        h=(h*60)+m
+                        printf("%d\n",h)
+                     }
+            }
+    }
+}
+
+
+return 0;
+
